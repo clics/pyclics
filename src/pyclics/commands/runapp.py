db90ad997e6139549f6de0f29610a98e35f9a6b9
xrotwang <xrotwang@googlemail.com>
2019-11-08 16:44:06 +0100

    closes #26

---
+++ src/pyclics/commands/runapp.py
@@ -0,0 +1,43 @@
+"""
+Run and open the local CLICS app in a browser.
+
+Stop with CTRL-q
+"""
+import argparse
+import subprocess
+import webbrowser
+
+from clldutils.clilib import confirm
+
+
+def register(parser):  # pragma: no cover
+    parser.add_argument(
+        '--port',
+        help='Port number to server the app on',
+        default=8000,
+        type=int,
+    )
+
+
+def run(args):  # pragma: no cover
+    appdir = args.repos.repos / 'app'
+    if not list(appdir.joinpath('cluster').iterdir()):
+        raise argparse.ArgumentError(
+            None,
+            'There are no clusters in {0}!\nYou may have to run "clics cluster" first'.format(
+                appdir))
+    proc = subprocess.Popen(
+        ['python', '-u', '-m', 'http.server', str(args.port)],
+        cwd=str(args.repos.repos / 'app'),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
+    url = 'http://localhost:{0}'.format(args.port)
+    webbrowser.open(url)
+    print('You should now see {0} opened in yur browser. If you are done using the app, press '
+          'enter'.format(url))
+    try:
+        if confirm('quit?'):
+            proc.terminate()
+    finally:
+        proc.terminate()
