d75b54cbb0332bc80b72a1a85f2d496efdaa929f
Mattis List <mattis.list@uni-passau.de>
2024-12-18 12:05:54 +0100

    release 3.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyclics',
-    version='3.0.3',
+    version='3.1.0',
     description="creating colexification networks from lexical data",
     long_description=open("README.md").read(),
     long_description_content_type='text/markdown',
